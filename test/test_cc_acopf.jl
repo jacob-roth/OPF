ab873cce6c216056f9644e331b5118434dff1460
Jake Roth <jmroth@uchicago.edu>
2019-04-22 13:42:28 -0500

    add Gamma_type (d,y) and xtilde(true,false) tests and functinality

--- test/test_cc_acopf.jl
+++ test/test_cc_acopf.jl
@@ -1,23 +1,33 @@
 @testset "cc-acopf" begin
-## data
-data = Dict()
-data[:Sigma_d]           = Matrix(Diagonal(ones(2nbus)))
-data[:Va_min]            = -pi * ones(nbus)
-data[:Va_max]            =  pi * ones(nbus)
+for Gamma_type = [:y, :d]
+    for xtilde = [true, false]
+        ## data
+        data = Dict()
+        data[:Sigma_d]           = Matrix(Diagonal(ones(2nbus)))
+        data[:Va_min]            = -pi * ones(nbus)
+        data[:Va_max]            =  pi * ones(nbus)
 
-## options
-options = Dict()
-options[:lossless]       = false
-options[:current_rating] = false
-options[:epsilon_Vm]     = 0.01
-options[:epsilon_Va]     = 0.05
-options[:epsilon_Qg]     = 0.05
-options[:gamma]          = 10.0
-options[:relax_Gamma]    = false
-options[:print_level]    = 5
+        ## options
+        options = Dict()
+        options[:lossless]       = false
+        options[:current_rating] = false
+        options[:epsilon_Vm]     = 0.01
+        options[:epsilon_Va]     = 0.05
+        options[:epsilon_Qg]     = 0.05
+        options[:gamma]          = 1.0
+        options[:relax_Gamma]    = false
+        options[:Gamma_type]     = Gamma_type
+        options[:xtilde]         = xtilde
+        options[:print_level]    = 5
 
-cm = OPF.cc_acopf_model(opfdata, options, data)
-cm = OPF.cc_acopf_solve(cm, opfdata)
+        cm = OPF.cc_acopf_model(opfdata, options, data)
+        cm = OPF.cc_acopf_solve(cm, opfdata, options)
 
-@test norm(getvalue(cm.m[:Gamma]) - (getvalue(cm.m[:dF_dx]) \ -getvalue(cm.m[:dF_dy]))) <= tol
+        if Gamma_type == :y
+            @test norm(getvalue(cm.m[:Gamma]) - (getvalue(cm.m[:dF_dx]) \ -getvalue(cm.m[:dF_dy]))) <= tol
+        else
+            @test norm(getvalue(cm.m[:Gamma]) - (getvalue(cm.m[:dF_dx]) \ -getvalue(cm.m[:dF_dd]))) <= tol
+        end
+    end
+end
 end # testset
