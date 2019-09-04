1d83a4910e32c6f826299badf178d5c17801258e
Jake Roth <jmroth@uchicago.edu>
2019-09-04 17:04:56 -0500

    add plotting to test for zip

--- test/test_zip.jl
+++ test/test_zip.jl
@@ -27,10 +27,17 @@ end # testset
 β = (1.0 .- α) ./ 2.0
 γ = (1.0 .- α) ./ 2.0
 @assert( α .+ β .+ γ == ones(length(ϵ)))
-obj = zeros(length(ϵ))
-opt = Array{Symbol}(undef, length(ϵ))
+obj     = zeros(length(ϵ))
+opt     = Array{Symbol}(undef, length(ϵ))
+ang_mu  = zeros(length(ϵ))
+mag_mu  = zeros(length(ϵ))
+Pg_mu   = zeros(length(ϵ))
+ang_sig = zeros(length(ϵ))
+mag_sig = zeros(length(ϵ))
+Pg_sig  = zeros(length(ϵ))
+
 for i in eachindex(α)
-    D = DefaultOptions()
+    D = OPF.DefaultOptions()
     D[:zip][:alpha] = α[i]
     D[:zip][:beta]  = β[i]
     D[:zip][:gamma] = γ[i]
@@ -41,8 +48,39 @@ for i in eachindex(α)
     OPF.acopf_outputAll(z, opfdata, D)
     obj[i] = getobjectivevalue(z.m)
     opt[i] = z.status
+    ang_mu[i]  = mean(abs.(getvalue(z.m[:Va])))
+    mag_mu[i]  = mean(getvalue(z.m[:Vm]))
+    Pg_mu[i]   = mean(getvalue(z.m[:Pg]))
+    ang_sig[i] = std(abs.(getvalue(z.m[:Va])))
+    mag_sig[i] = std(getvalue(z.m[:Vm]))
+    Pg_sig[i]  = std(getvalue(z.m[:Pg]))
 end
-# semilogx(ϵ, obj)
+
+if plotting
+    using PyPlot
+    rcParams = PyPlot.matplotlib[:rcParams]
+    prop_cycle = rcParams["axes.prop_cycle"]
+    colors = prop_cycle[:by_key]()["color"]
+
+    fig = subplots()
+    ax  = subplot(111)
+    ax[:semilogx](ϵ, obj, label = "objective value")
+    ax[:set_xlabel]("ϵ")
+    ax[:set_ylabel]("objective value")
+    ax[:legend]()
+
+    fig = subplots()
+    ax  = subplot(111)
+    ax[:loglog](ϵ, ang_mu, label="mean(θ)", color=colors[1], alpha=0.5)
+    ax[:loglog](ϵ, ang_sig, label="std(θ)", color=colors[1], alpha=0.5, linestyle=":")
+    ax[:loglog](ϵ, mag_mu, label="mean(V)", color=colors[2], alpha=0.5)
+    ax[:loglog](ϵ, mag_sig, label="std(V)", color=colors[2], alpha=0.5, linestyle=":")
+    ax[:loglog](ϵ, Pg_mu, label="mean(Pg)", color=colors[3], alpha=0.5)
+    ax[:loglog](ϵ, Pg_sig, label="std(Pg)", color=colors[3], alpha=0.5, linestyle=":")
+    ax[:set_xlabel]("ϵ")
+    ax[:set_ylabel]("objective value")
+    ax[:legend]()
+end
 
 @testset "zip optimality" begin
 for i in eachindex(ϵ)
