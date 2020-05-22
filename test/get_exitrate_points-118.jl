5808c1fc3d87efc555bbc49c9af9423da8e188bd
Jake Roth <jmroth@uchicago.edu>
2020-05-22 10:02:31 -0500

    118 gen points

--- test/get_exitrate_points-118.jl
+++ test/get_exitrate_points-118.jl
@@ -13,6 +13,8 @@ using DelimitedFiles
 
 fileout = "/home/jroth/Projects/planning-large-deviation/data/optimalvalues/118bus_base/"
 case_name = "mpc_base_pgliblimits"
+fileout = "/home/jroth/Projects/planning-large-deviation/data/optimalvalues/118bus_lowdamp/"
+case_name = "mpc_lowdamp_pgliblimits"
 
 options = DefaultOptions()
 options[:emergencylimit] = NaN
@@ -37,8 +39,6 @@ function set_optimalvalues(case_name::String, constr_limit_scales::Array{T,1}, r
     opfmodel = acopf_model(opfdata, options)
     opfmodel_acopf = acopf_solve(opfmodel, opfdata)
     acopf_outputAll(opfmodel_acopf, opfdata, options)
-    # 118-n1-lowdamp: Objective value: 125947.8781174833. Generation cost = 125947.87945987795USD/hr
-    # 30-mpc-lowdamp: Objective value: 604.249424491365. Generation cost = 604.249424491365USD/hr
 
     ##
     ## exitrates
@@ -57,12 +57,6 @@ function set_optimalvalues(case_name::String, constr_limit_scales::Array{T,1}, r
             opfmodel_exitrates = acopf_solve_exitrates(opfmodel, casedata, options)
             push!(j_models, opfmodel_exitrates)
 
-            # rates = zeros(3, 41)
-            # m = opfmodel_exitrates
-            # for i in 1:41
-            #     rates[:,i] .= [m.other[:prefactors][i]; m.other[:expterms][i]; m.other[:rates][i]]
-            # end
-
             acopf_outputAll(opfmodel_exitrates, opfdata, options) # Objective value: 125947.87811710747. Generation cost = 125947.87948380895USD/hr
             optimal_values = get_optimal_values(opfmodel_exitrates.m, get_opfmodeldata(opfdata, options))
             optimal_values[:rates] = opfmodel_exitrates.other[:rates]
@@ -80,6 +74,11 @@ end
 
 constr_limit_scales = [1.04] #round.(collect(range(1.0, stop=1.1, step=0.02)).^2, digits=2)
 ratelimits = [Inf, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11] #[Inf; exp10.(collect(range(3, stop=-4, step=-1)))]
+models1 = set_optimalvalues(case_name, constr_limit_scales[1:1], ratelimits, options, fileout)
+ratelimits = [1e-12, 1e-16, 1e-20, 1e-24, 1e-28, 1e-30]
+models2 = set_optimalvalues(case_name, constr_limit_scales[1:1], ratelimits, options, fileout)
+
+ratelimits = [1e-30, 1e-40, 1e-50, 1e-60]
 models = set_optimalvalues(case_name, constr_limit_scales[1:1], ratelimits, options, fileout)
 
 # set_optimalvalues(case_name, constr_limit_scales[1:end], ratelimits[1:end], options, fileout)
@@ -87,3 +86,5 @@ sumrates = zeros(length(models[1]))
 for i in eachindex(models[1])
     sumrates[i] = sum(models[1][i].other[:rates])
 end
+
+## mpc_base_pgliblimits: 126423.58585637706
