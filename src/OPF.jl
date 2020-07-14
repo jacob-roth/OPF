3c79d42da5bf504a59faea5143abc3e896e9ebf2
Albert Lam <albert233@hotmail.com>
2020-07-14 10:18:45 -0500

    Added acpf_model_Pg to OPF.jl

--- src/OPF.jl
+++ src/OPF.jl
@@ -27,6 +27,9 @@ export add_line_current_constraint!, add_line_power_constraint!
 include("acpf_model.jl")
 export acpf_model
 
+include("acpf_model_Pg.jl")
+export acpf_model
+
 include("acopf_model.jl")
 export acopf_model
 
