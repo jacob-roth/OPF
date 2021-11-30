0e4215264b7018a40d3024e31b167471aa365fe6
jake <rothjakem@gmail.com>
2021-11-30 17:08:51 -0600

    pg utils update

--- src/powergrid_utils.jl
+++ src/powergrid_utils.jl
@@ -37,12 +37,44 @@ function import_own_operatingpoint(powergrid::PowerGrid, operatingdata_path::Str
 
     u_r = Vm .* cos.(Va)
     u_i = Vm .* sin.(Vm)
+    ω = 0 #2 * π * 50
+
+    own_op = Array{Float64,1}(undef, num_vars)
+    vec_idx = 1
+    for node_idx = 1:length(nodes)
+        node = nodes["bus$node_idx"]
+        if isa(node, SwingEq)
+            own_op[vec_idx] = u_r[node_idx]
+            own_op[vec_idx+1] = u_i[node_idx]
+            own_op[vec_idx+2] = ω
+            vec_idx += 3
+        else
+            own_op[vec_idx] = u_r[node_idx]
+            own_op[vec_idx+1] = u_i[node_idx]
+            vec_idx += 2
+        end
+    end
+    return own_op
+end
+
+# Import operatingpoint from FPACOPF into PowerDynamics framework
+function import_own_operatingpoint(powergrid::PowerGrid, optimal_values::Dict)
+    nodes = powergrid.nodes
+    num_buses = length(nodes)
+    type_dict = get_type_dict(powergrid)
+    num_vars = get_num_vars(type_dict)
+
+    Va = optimal_values[:Va]
+    Vm = optimal_values[:Vm]
+
+    u_r = Vm .* cos.(Va)
+    u_i = Vm .* sin.(Vm)
     ω = 0
 
     own_op = Array{Float64,1}(undef, num_vars)
     vec_idx = 1
-    for node_idx in 1:length(nodes)
-        node = nodes[node_idx]
+    for node_idx = 1:length(nodes)
+        node = nodes["bus$node_idx"]
         if isa(node, SwingEq)
             own_op[vec_idx] = u_r[node_idx]
             own_op[vec_idx+1] = u_i[node_idx]
