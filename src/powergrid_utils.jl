6ff9b10808c3e5271381956d3f9f476d964c9eab
Albert Lam <albert233@hotmail.com>
2021-12-02 23:42:51 -0600

    Updated import_own_operatingpoint for current JSON format

--- src/powergrid_utils.jl
+++ src/powergrid_utils.jl
@@ -27,38 +27,6 @@ function get_num_vars(type_dict::Dict{Type{<:AbstractNode}, Int})
 end
 
 # Import operatingpoint from FPACOPF into PowerDynamics framework
-function import_own_operatingpoint(powergrid::PowerGrid, operatingdata_path::String)
-    nodes = powergrid.nodes
-    num_buses = length(nodes)
-    type_dict = get_type_dict(powergrid)
-    num_vars = get_num_vars(type_dict)
-
-    Va = reshape(readdlm(operatingdata_path * "Va.csv"), num_buses)
-    Vm = reshape(readdlm(operatingdata_path * "Vm.csv"), num_buses)
-
-    u_r = Vm .* cos.(Va)
-    u_i = Vm .* sin.(Vm)
-    ω = 0
-
-    own_op = Array{Float64,1}(undef, num_vars)
-    vec_idx = 1
-    for node_idx = 1:length(nodes)
-        node = nodes[node_idx]
-        if isa(node, SwingEq)
-            own_op[vec_idx] = u_r[node_idx]
-            own_op[vec_idx+1] = u_i[node_idx]
-            own_op[vec_idx+2] = ω
-            vec_idx += 3
-        else
-            own_op[vec_idx] = u_r[node_idx]
-            own_op[vec_idx+1] = u_i[node_idx]
-            vec_idx += 2
-        end
-    end
-    return own_op
-end
-
-# Import operatingpoint from FPACOPF into PowerDynamics framework
 function import_own_operatingpoint(powergrid::PowerGrid, optimal_values::Dict)
     nodes = powergrid.nodes
     type_dict = get_type_dict(powergrid)
@@ -87,6 +55,17 @@ function import_own_operatingpoint(powergrid::PowerGrid, optimal_values::Dict)
         end
     end
     return own_op
+end
+
+function import_own_operatingpoint(powergrid::PowerGrid, operatingdata_path::String)
+    nodes = powergrid.nodes
+    num_buses = length(nodes)
+
+    optimal_values = Dict()
+    optimal_values[:Va] = reshape(readdlm(operatingdata_path * "Va.csv"), num_buses)
+    optimal_values[:Vm] = reshape(readdlm(operatingdata_path * "Vm.csv"), num_buses)
+
+    return import_own_operatingpoint(powergrid, optimal_values)
 end
 
 function isa_root(powergrid::PowerGrid, operatingpoint_vec::AbstractArray; abs_err::Real=eps())
