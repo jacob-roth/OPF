36ba4ea34d12574ad5a446e40c56f4980ad6e1ff
jake <rothjakem@gmail.com>
2021-12-05 22:24:50 -0600

    include albert's y_shunt_km

--- src/OPF2PD.jl
+++ src/OPF2PD.jl
@@ -55,6 +55,7 @@ function opf2pd(fileout::String, optimal_values::Dict, opfmodeldata::Dict, line_
     push!(out["nodes"], B)
   end
 
+  ## best so far
   # lines
   from_to_pairs = Set{Tuple{Int, Int}}()
   for l = 1:nline
@@ -62,6 +63,7 @@ function opf2pd(fileout::String, optimal_values::Dict, opfmodeldata::Dict, line_
     t = opfmodeldata[:lines][l].to
     ff = min(f,t) # need to have from.id <= to.id for PD lightgraphs dependency
     tt = max(f,t) # need to have from.id <= to.id for PD lightgraphs dependency
+    # good_order = (t == tt) * (f == ff)
 
     if (ff,tt) ∈ from_to_pairs
       continue
@@ -77,17 +79,29 @@ function opf2pd(fileout::String, optimal_values::Dict, opfmodeldata::Dict, line_
     L["params"]["to"] = "bus" * string(tt)
     L["type"] = line_type
     if line_type == "PiModelLine"
-      y = Ys = 1/(lines[l].x*im)
-      y_shunt_km = im*(lines[l].b/2 + (buses[ff].Bs / baseMVA))
-      y_shunt_mk = im*(lines[l].b/2 + (buses[tt].Bs / baseMVA))
+      # y = Ys = 1/(lines[l].x*im)
+      # y_shunt_km = im*(lines[l].b/2 + (buses[ff].Bs / baseMVA))
+      # y_shunt_mk = im*(lines[l].b/2 + (buses[tt].Bs / baseMVA))
+      from_bus = f # are these correct or do they need to depend on whether t,f are swapped
+      to_bus = t # are these correct or do they need to depend on whether t,f are swapped
+    
+      line_b = sum(lines[(lines.from .== from_bus) .& (lines.to .== to_bus)].b) # duplicate line shunts
+      from_Bs = first(buses[buses.bus_i .== from_bus].Bs) # don't sum bus shunts
+      to_Bs = first(buses[buses.bus_i .== to_bus].Bs) # don't sum bus shunts
+      # from_Bs = sum(buses[buses.bus_i .== from_bus].Bs)
+      # to_Bs = sum(buses[buses.bus_i .== to_bus].Bs)
+      y_shunt_km = im*(line_b / 2 + (from_Bs / baseMVA))
+      y_shunt_mk = im*(line_b / 2 + (to_Bs / baseMVA))
+      
+
       L["params"]["y"] = Dict()
-      L["params"]["y"]["re"] = +1real(y) # -1 +1 -1 +1
-      L["params"]["y"]["im"] = +1imag(y) # +1 +1 -1 -1
+      L["params"]["y"]["re"] = 0.0
+      # L["params"]["y"]["im"] = imag(Ys)
       L["params"]["y_shunt_km"] = +1y_shunt_km # -1 +1 -1 +1
       L["params"]["y_shunt_mk"] = +1y_shunt_mk # +1 +1 -1 -1
       # L["params"]["y"] = Dict()
       # L["params"]["y"]["re"] = 0.0
-      # L["params"]["y"]["im"] = -opfmodeldata[:Y][ff, tt]
+      L["params"]["y"]["im"] = -opfmodeldata[:Y][ff, tt]
       # L["params"]["y_shunt_km"] = complex(0.0, opfmodeldata[:YshI][ff])
       # L["params"]["y_shunt_mk"] = complex(0.0, opfmodeldata[:YshI][tt])
     elseif line_type == "PiModelLineTapratio"
@@ -170,3 +184,69 @@ function opf2pd(fileout::String, casedata_path::String, operatingdata_path::Stri
   optimal_values[:Qnet] = Qnet
   return opf2pd(fileout, optimal_values, opfmodeldata, line_type, phys)
 end
+
+
+
+
+
+
+
+  # # lines
+  # for l = 1:nline
+  #   f = opfmodeldata[:lines][l].from
+  #   t = opfmodeldata[:lines][l].to
+  #   ff = min(f,t) # need to have from.id <= to.id for PD lightgraphs dependency
+  #   tt = max(f,t) # need to have from.id <= to.id for PD lightgraphs dependency
+  #   good_order = (ff==f)*(tt==t)
+    
+  #   L = Dict()
+  #   L["name"] = "branch" * string(l)
+  #   L["params"] = Dict()
+  #   L["params"]["from"] = "bus" * string(ff)
+  #   L["params"]["to"] = "bus" * string(tt)
+  #   L["type"] = line_type
+  #   if line_type == "PiModelLine"
+  #     y = Ys = 1/(lines[l].x*im)
+  #     y_shunt_km = im*(lines[l].b/2 + (buses[ff].Bs / baseMVA))
+  #     y_shunt_mk = im*(lines[l].b/2 + (buses[tt].Bs / baseMVA))
+  #     from_bus = f
+  #     to_bus = t
+    
+  #     line_b = sum(lines[(lines.from .== from_bus) .& (lines.to .== to_bus)].b) # duplicate line shunts
+  #     from_Bs = first(buses[buses.bus_i .== from_bus].Bs) # don't sum bus shunts
+  #     to_Bs = first(buses[buses.bus_i .== to_bus].Bs) # don't sum bus shunts
+  #     # from_Bs = sum(buses[buses.bus_i .== from_bus].Bs)
+  #     # to_Bs = sum(buses[buses.bus_i .== to_bus].Bs)
+  #     y_shunt_km = im*(line_b / 2 + (from_Bs / baseMVA))
+  #     y_shunt_mk = im*(line_b / 2 + (to_Bs / baseMVA))
+      
+
+  #     L["params"]["y"] = Dict()
+  #     L["params"]["y"]["re"] = 0.0
+  #     # L["params"]["y"]["im"] = imag(Ys)
+  #     L["params"]["y_shunt_km"] = +1y_shunt_km # -1 +1 -1 +1
+  #     L["params"]["y_shunt_mk"] = +1y_shunt_mk # +1 +1 -1 -1
+  #     # L["params"]["y"] = Dict()
+  #     # L["params"]["y"]["re"] = 0.0
+  #     L["params"]["y"]["im"] = -opfmodeldata[:Y][ff, tt]
+  #     # L["params"]["y_shunt_km"] = complex(0.0, opfmodeldata[:YshI][ff])
+  #     # L["params"]["y_shunt_mk"] = complex(0.0, opfmodeldata[:YshI][tt])
+  #   elseif line_type == "PiModelLineTapratio"
+  #     L["params"]["y"] = Dict()
+  #     L["params"]["y"]["re"] = 0.0
+  #     L["params"]["y"]["im"] = -opfmodeldata[:Y][ff, tt]
+  #     L["params"]["y_shunt_km"] = complex(0.0, opfmodeldata[:YshI][ff])
+  #     L["params"]["y_shunt_mk"] = complex(0.0, opfmodeldata[:YshI][tt])
+  #     tap = (opfmodeldata[:lines][l].ratio == 0) ? (1.0) : (opfmodeldata[:lines][l].ratio)
+  #     tap *= exp(opfmodeldata[:lines][l].angle * pi/180 * im)
+  #     L["params"]["t_shunt_km"] = tap
+  #     L["params"]["t_shunt_mk"] = tap
+  #   else
+  #     L["params"]["Y"] = Dict()
+  #     L["params"]["Y"]["re"] = 0.0
+  #     L["params"]["Y"]["im"] = -opfmodeldata[:Y][ff, tt]
+  #   end
+
+  #   # add to `nodes` array
+  #   push!(out["lines"], L)
+  # end
